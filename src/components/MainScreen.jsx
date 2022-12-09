6f9ef33d7e0aaa7172c6d645a7aa4b850d082faf
Максим Стрельников <maksim_stv@mail.ru>
2022-12-09 11:21:45 +0300

    full-3

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -102,7 +102,7 @@ const MainScreen = function () {
       queryId: tg.initDataUnsafe?.query_id,
 
     }
-    fetch('http://5.61.44.64:8000/web-data', {
+    fetch('http://192.168.0.2:8000/web-data', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
